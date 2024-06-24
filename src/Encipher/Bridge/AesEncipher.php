1a672e33dc2ef136c20c3609ab6b24f8253609b3
crayxn <so.wo@foxmail.com>
2024-06-24 15:26:21 +0800

    fix：优化加解密

--- src/Encipher/Bridge/AesEncipher.php
+++ src/Encipher/Bridge/AesEncipher.php
@@ -2,27 +2,16 @@
 
 namespace Xhtkyy\Helper\Encipher\Bridge;
 
-use Exception;
-use Hyperf\Contract\ConfigInterface;
 use Xhtkyy\Helper\Encipher\EncipherInterface;
 
 class AesEncipher implements EncipherInterface {
 
-    private ?string $encipher_secret;
-    private ?string $encipher_iv;
-    private string $encipher_code = 'UTF-8';
-    private string $encipher_algo = 'AES-256-CBC';
-    private string $encipher_delimiter = '.';
-
-    /**
-     * @throws Exception
-     */
-    public function __construct(protected ConfigInterface $config) {
-        $this->encipher_secret = $this->config->get('kyy_security.encipher_secret');
-        $this->encipher_iv     = $this->config->get('kyy_security.encipher_iv');
-        if (empty($this->encipher_secret) || empty($this->encipher_iv)) {
-            throw new Exception('Encipher secret and iv are required.');
-        }
+    public function __construct(
+        private string $encipher_secret,
+        private string $encipher_iv,
+        private string $encipher_algo = 'AES-256-CBC',
+        private string $encipher_delimiter = '.'
+    ) {
     }
 
     /**
@@ -33,18 +22,14 @@ class AesEncipher implements EncipherInterface {
      */
     public function encrypt(string $plaintext, int $length): string {
         $groups = [];
-        $len    = mb_strlen($plaintext, $this->encipher_code);
+        $len    = mb_strlen($plaintext);
         if ($len <= $length) {
             return openssl_encrypt($plaintext, $this->encipher_algo, $this->encipher_secret, 0, $this->encipher_iv);
         }
         for ($i = 0; $i <= $len - $length; $i++) {
-            $groups[] = sprintf(
-                "%s%s",
-                openssl_encrypt(mb_substr($plaintext, $i, $length, $this->encipher_code), $this->encipher_algo, $this->encipher_secret, 0, $this->encipher_iv),
-                $this->encipher_delimiter
-            );
+            $groups[] = openssl_encrypt(mb_substr($plaintext, $i, $length), $this->encipher_algo, $this->encipher_secret, 0, $this->encipher_iv);
         }
-        return implode('', $groups);
+        return implode($this->encipher_delimiter, $groups);
     }
 
     /**
@@ -54,7 +39,7 @@ class AesEncipher implements EncipherInterface {
      * @return string
      */
     public function decrypt(string $encrypted, int $length): string {
-        if (mb_strlen($encrypted, $this->encipher_code) <= $length) {
+        if (mb_strlen($encrypted) <= $length) {
             // 如果加密数据长度小于或等于分组长度，直接解密
             return openssl_decrypt($encrypted, $this->encipher_algo, $this->encipher_secret, 0, $this->encipher_iv);
         }
@@ -65,26 +50,10 @@ class AesEncipher implements EncipherInterface {
         foreach (array_filter($encryptedGroups) as $key => $encryptedGroup) {
             // 对每个分组进行解密
             $decryptedGroup = openssl_decrypt($encryptedGroup, $this->encipher_algo, $this->encipher_secret, 0, $this->encipher_iv);
-            if ($key == 0) {
-                $plaintext = $decryptedGroup;
-                continue;
-            }
-            $plaintext .= $this->getLastCharacter($decryptedGroup); // 将解密后的分组拼接起来
+            $plaintext      .= ($key == 0 ? $decryptedGroup : mb_substr($decryptedGroup, -1)); // 将解密后的分组拼接起来
         }
 
         // 去除解密后字符串末尾可能出现的填充字符
-        return rtrim($plaintext, "\0");
-    }
-
-    /**
-     * 获取字符串最后一个字符
-     * @param $str
-     * @return string|null
-     */
-    private function getLastCharacter($str): ?string {
-        if (mb_strlen($str, $this->encipher_code) > 0) {
-            return mb_substr($str, mb_strlen($str, $this->encipher_code) - 1, 1, $this->encipher_code);
-        }
-        return null;
+        return $plaintext;
     }
 }
--- src/Encipher/Facades/EncipherFactory.php
+++ src/Encipher/Facades/EncipherFactory.php
@@ -5,11 +5,18 @@ namespace Xhtkyy\Helper\Encipher\Facades;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\ContainerInterface;
 use Xhtkyy\Helper\Encipher\Bridge\AesEncipher;
+use Xhtkyy\Helper\Encipher\EncipherInterface;
 
 class EncipherFactory {
 
-    public function __invoke(ContainerInterface $container) {
-        return $container->make(AesEncipher::class);
+    public function __invoke(ContainerInterface $container): EncipherInterface {
+        $config          = $container->get(ConfigInterface::class);
+        $encipher_secret = $config->get('kyy_security.encipher_secret');
+        $encipher_iv     = $config->get('kyy_security.encipher_iv');
+        if (empty($encipher_secret) || empty($encipher_iv)) {
+            throw new \Exception('Encipher secret and iv are required.');
+        }
+        return new AesEncipher($encipher_secret, $encipher_iv);
     }
 
 }
